e2cbe6851890adec44469eaa73857d7cf8ac8ba9
pekpuglia <pepuglia@gmail.com>
2023-07-18 15:49:53 -0300

    refactor complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-use ode_solvers::{self, System, DVector};
+#![feature(generic_const_exprs)]
+use ode_solvers::{self, DVector, SVector};
+use nalgebra::{Vector, Vector2, ArrayStorage, ToConst};
 
-trait DynamicalSystem {
-    const STATE_VECTOR_SIZE: usize;
-    type Input;
-    type Output;
+trait DynamicalSystem<
+    const StateVectorSize: usize,
+    const InputSize      : usize,
+    const OutputSize     : usize> {
 
-    fn xdot(&self, t: f64, x: DVector<f64>, u: Self::Input) -> DVector<f64>;
-    fn y(&self, t: f64, x: DVector<f64>, u: Self::Input) -> Self::Output;
+    fn xdot(&self, t: f64, 
+        x: SVector<f64, StateVectorSize>, 
+        u: SVector<f64, InputSize>) -> SVector<f64, StateVectorSize>;
+    fn y(&self, t: f64, 
+        x: SVector<f64, StateVectorSize>, 
+        u: SVector<f64, InputSize>) -> SVector<f64, OutputSize>;
 }
 
 mod series;
