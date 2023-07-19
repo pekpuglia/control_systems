8842b6163f1b88d64dc10ff5b8f0c4624d06573d
pekpuglia <pepuglia@gmail.com>
2023-07-19 20:24:24 -0300

    lib refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
-#![feature(generic_const_exprs)]
-pub use nalgebra::SVector;
+pub use nalgebra::DVector;
 
 //fix warnings
 //system lib?
 
 //problema: pode ter mais de 1 implementação por tipo p parâmetros genéricos diferentes
-pub trait DynamicalSystem<
-    const STATE_VECTOR_SIZE: usize,
-    const INPUT_SIZE      : usize,
-    const OUTPUT_SIZE     : usize> {
+pub trait DynamicalSystem {
+
+    const STATE_VECTOR_SIZE: usize;
+    const INPUT_SIZE      : usize;
+    const OUTPUT_SIZE     : usize;
 
     fn xdot(&self, t: f64, 
-        x: SVector<f64, STATE_VECTOR_SIZE>, 
-        u: SVector<f64, INPUT_SIZE>) -> SVector<f64, STATE_VECTOR_SIZE>;
+        x: DVector<f64>, 
+        u: DVector<f64>) -> DVector<f64>;
     fn y(&self, t: f64, 
-        x: SVector<f64, STATE_VECTOR_SIZE>, 
-        u: SVector<f64, INPUT_SIZE>) -> SVector<f64, OUTPUT_SIZE>;
+        x: DVector<f64>, 
+        u: DVector<f64>) -> DVector<f64>;
 }
 
 mod series;
 pub use series::Series;
 
 
-mod feedback;
-pub use feedback::NegativeFeedback;
+// mod feedback;
+// pub use feedback::{NegativeFeedback, UnitySystem};
