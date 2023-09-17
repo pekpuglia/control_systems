b6285a05fb2428be927004ae067b4548505ea789
pekpuglia <pepuglia@gmail.com>
2023-09-17 21:25:21 +0200

    from impl

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ pub trait DynamicalSystem {
         u: DVector<f64>) -> DVector<f64>;
 }
 
+#[derive(Clone, Debug)]
 pub struct StateVector<System: DynamicalSystem> {
     pub data: DVector<f64>,
     _phantom: PhantomData<System>
@@ -37,6 +38,12 @@ impl<System: DynamicalSystem> StateVector<System>  {
         assert!(x.len() == System::STATE_VECTOR_SIZE);
         StateVector { data: DVector::from_row_slice(x), 
             _phantom: PhantomData }
+    }
+}
+
+impl<System: DynamicalSystem> From<DVector<f64>> for StateVector<System> {
+    fn from(value: DVector<f64>) -> Self {
+        StateVector { data: value, _phantom: PhantomData }
     }
 }
 
