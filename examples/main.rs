8989078e6a33b29822d23774a152b155a9bef998
pekpuglia <pepuglia@gmail.com>
2024-03-30 21:40:53 +0100

    main updated

--- examples/main.rs
+++ examples/main.rs
@@ -1,40 +1,11 @@
-use control_systems::{DynamicalSystem, DVector, dvector, UnityFeedback, StateVector, IntoSV, NegativeFeedback};
-
-#[derive(Clone, Copy)]
-struct SecondOrder {
-    k: f64,
-    c: f64
-}
-
-impl DynamicalSystem for SecondOrder {
-    fn xdot(&self, _t: f64, 
-        x: &StateVector<Self>, 
-        u: DVector<f64>) -> DVector<f64> {
-        dvector![
-            x[1],
-            -self.k*x[0]-self.c*x[1]+u[0]
-        ]
-    }
-
-    fn y(&self, _t: f64, 
-        x: &StateVector<Self>, 
-        _u: DVector<f64>) -> DVector<f64> {
-        dvector![x[0]]
-    }
-
-    const STATE_VECTOR_SIZE: usize = 2;
-
-    const INPUT_SIZE      : usize = 1;
-
-    const OUTPUT_SIZE     : usize = 1;
-}
-
+use control_systems::{dvector, systems::SecondOrder, DVector, DynamicalSystem, NegativeFeedback, UnityFeedback, VecConcat};
+use nalgebra::{vector, Vector1, Vector2};
 fn main() {
-    let test = SecondOrder{ k: 1.0, c: 1.0 };
+    let test = SecondOrder::new(1.0, 1.0);
 
-    let feedback1 = UnityFeedback::<SecondOrder, 1>::new_unity_feedback( test);
+    let feedback1 = UnityFeedback::<Vector1<f64>, Vector2<f64>, SecondOrder>::new_unity_feedback( test);
 
-    let output = feedback1.y(0.0, &[0.0, 1.0, 0.0, 1.0].into_sv::<UnityFeedback<SecondOrder, 1>>(), dvector![1.0]);
+    let output = feedback1.y(0.0, &VecConcat::new(vector![0.0, 1.0], vector![]), &vector![1.0]);
 
     println!("{}", output);
 }
