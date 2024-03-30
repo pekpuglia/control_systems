ad4784cba9a5b496e9cc6e3f34ef09518ad1e597
pekpuglia <pepuglia@gmail.com>
2024-03-30 19:52:54 +0100

    added conversion to dvector and subtraction

--- src/state_vector.rs
+++ src/state_vector.rs
@@ -1,3 +1,5 @@
+use std::{ops::Sub, process::Output};
+
 use nalgebra::SVector;
 
 use crate::*;
@@ -5,15 +7,22 @@ use crate::*;
 //fazer conversão para DVector
 //igualdade!!!
 //indexação!!!
-pub trait ComposableVector {
+pub trait ComposableVector: Sub<Output=Self> + Sized + Copy {
     fn concat<CVO: ComposableVector>(self, other: CVO) -> VecConcat<Self, CVO> where Self: Sized {
         VecConcat(self, other)
     }
+
+    fn into_dvector(&self) -> DVector<f64>;
 }
 
 
-impl<const N: usize> ComposableVector for SVector<f64, N> {}
+impl<const N: usize> ComposableVector for SVector<f64, N> {
+    fn into_dvector(&self) -> DVector<f64> {
+        DVector::from_row_slice(self.data.as_slice())
+    }
+}
 
+#[derive(Clone, Copy, Debug)]
 pub struct VecConcat<CV0: ComposableVector, CV1: ComposableVector>(CV0, CV1);
 
 impl<CV0: ComposableVector, CV1: ComposableVector> VecConcat<CV0, CV1> {
@@ -30,8 +39,20 @@ impl<CV0: ComposableVector, CV1: ComposableVector> VecConcat<CV0, CV1> {
     }
 }
 
+impl<CV0: ComposableVector , CV1: ComposableVector> Sub for VecConcat<CV0, CV1> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        VecConcat::new(self.0 - rhs.0, self.1-rhs.1)
+    }
+}
+
 impl<CV0: ComposableVector, CV1: ComposableVector> ComposableVector for VecConcat<CV0, CV1> {
-    
+    fn into_dvector(&self) -> DVector<f64> {
+        let mut ret = self.0.into_dvector();
+        ret.extend(self.1.into_dvector().iter().copied());
+        ret
+    }
 }
 
 // #[derive(Clone, Debug)]
